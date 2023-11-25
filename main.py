270bbce75552f4a7ae750e7557354ef8c2d0b40a
Maxwell <130581484+Maxw311nyimbili@users.noreply.github.com>
2023-11-25 15:23:49 +0000

    Plot dummy data dynamically

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place.capitalize()}")
 
+# to draw the graph import plotly.express, then use plotly_chart()
+# you will also need a corresponding list of dates and temperatures and they should
+# be of the same length
+
+dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+temperature = [10, 11, 15]
+
+figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y" : "Temperature"})
+st.plotly_chart(figure)
